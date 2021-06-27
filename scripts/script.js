0697a968177773e006328cba5cd4e951e0ffab6f
Atul Saini <atulsaini9886@gmail.com>
2021-06-27 23:04:51 +0530

    Added Media Query

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ var create = document.getElementById("create-nav");
 var categoryHover = document.querySelector(".category-hover-list");
 var createHover = document.querySelector(".create-hover-list");
 
+var hamBtn = document.querySelector("#ham-btn");
+var nav = document.querySelector(".list");
+
 //Category Hovers handling
 category.onmouseover = function () {
   categoryHover.style.display = "grid";
@@ -37,3 +40,17 @@ create.onmouseleave = function () {
 createHover.onmouseleave = function () {
   createHover.style.display = "none";
 };
+
+//Handling the nav bar
+
+// hamBtn.addEventListener("click", );
+// hamBtn.onClick =
+// };
+function displayNav() {
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
+hamBtn.onclick = displayNav;
